8ce58969b6fb832db02a2c3835d1c6ba3eafc728
tushar <tusharkanti647@gmail.com>
2023-02-06 16:08:26 +0000

    Update package-lock.json and App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,9 @@ import React from "react"
 import '../styles/App.css'
 
 const App = ()=>{
-    return (<>
+    return (
        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-    </>);
+    );
 }
 
 export default App;
